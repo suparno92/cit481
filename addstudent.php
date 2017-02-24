5d91ecf1592c4f76440b82255fa417ff50920128
suparno92 <suparno.samanta.528@my.csun.edu>
2017-02-23 18:32:32 -0800

    Add files via upload

---
+++ addstudent.php
@@ -0,0 +1,29 @@
+<?php
+
+  session_start();
+
+ if(isset($_SESSION['username'])){
+
+  include('mysql_connection.php');
+
+
+   $student_id = $_SESSION['student_id'];
+    // trims any blank spaces and get the username and password
+   $clicked_id = $_POST['name'];
+
+
+   $addquery=$conn->prepare("INSERT INTO student_meetings (student_id,meeting_id) VALUES(?,?)");
+   $addquery->bind_param('ii',$student_id,$clicked_id);
+   $addquery->execute();
+   $addquery->close();
+
+   echo "You have been added to the meeting.";
+
+
+ } else{
+     echo "Please LOGIN to add.";
+
+
+ }
+
+?>
